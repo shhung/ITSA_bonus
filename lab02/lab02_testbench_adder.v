53fb0d715b525fda9f2e8facb33f34541e7262ac
shhung <53633033+shhung@users.noreply.github.com>
2020-03-23 15:45:50 +0800

    Update lab02_testbench_adder.v

--- lab02/lab02_testbench_adder.v
+++ lab02/lab02_testbench_adder.v
@@ -1,13 +1,15 @@
 `timescale 1ns / 1ps
-module testbench_adder;
 
-reg [3:0] a, b,c ,d;
+module testbench_4bit_adder;
+
+reg [3:0] a, b, c ,d;
 wire [3:0] sum;
 wire ov;
-reg clk;
-reg	rst;
-reg [6:0] correct_sum;
-reg [6:0] test_num;
+
+reg clk = 0;
+reg	rst = 0;
+reg [7:0] correct_sum;
+reg [7:0] test_num;
 reg correct_ov;
 
 always #5 clk = ~clk;
@@ -20,13 +22,14 @@ begin
 	clk <= 0;
 	rst <= 0;
 	a <= 0; 
-	b <= 0;
-	c <= 0;
+	b <= 0; 
+	c <= 0; 
 	d <= 0;
 	test_num <= 0;
 	$dumpfile("adder.vcd");
 	$dumpvars;
 end
+
 
 always@(posedge clk or negedge rst)
 begin
@@ -35,18 +38,22 @@ begin
 			a <= a + 1;
 			test_num <= test_num + 1;
 		end 
-		if(a ==15) begin
-			a <= 0;
+		if(a == 15) begin
 			b <= b + 1;
 		end
+		if(b == 15 && a == 15) begin
+			b <= b + 1;
+			c <= c + 1;
+		end
+		d <= {$random} % 16;
 	end
 	else begin
-		{correct_ov, correct_sum} = a + b;
+		{correct_ov, correct_sum} = a + b + c + d;
 		if({ov, sum} == {correct_ov, correct_sum}) begin
 			$display ("Test %d ", test_num);
 			$display ("OK!");
 			/*
-			$display ("%d + %d = ?", a, b);
+			$display ("%d + %d + %d + %d = ?", a, b, c, d);
 			$display ("your answer: ov = %d, sum = %d", ov, sum);
 			$display ("correct answer: ov = %d, sum = %d", ov, sum);
 			$display ("\n");
@@ -64,8 +71,6 @@ begin
 		end
 	end
 end
+
 initial #1005 $finish;
 endmodule
-
-
-
