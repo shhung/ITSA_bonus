1c775daeaea0f4c340c744b9ae4412fcdfbf5102
shhung <53633033+shhung@users.noreply.github.com>
2020-03-23 09:31:53 +0800

    Add files via upload

---
+++ lab02/adder.v
@@ -0,0 +1,10 @@
+module  adder(a, b, c, d, sum, ov);
+  input [3:0] a, b ,c ,d;
+  output [3:0] sum;
+  output ov;
+  wire s1 ,s2 ,s3;
+
+  assign {s1, s2, s3, sum} = a + b + c + d;
+  assign ov = 1'b0;
+
+endmodule 
